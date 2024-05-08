0b18354379981a840fb1fc07f39794369622dc13
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-05-08 15:04:22 -0400

    Add files via upload

--- telegram/js/scripttm.js
+++ telegram/js/scripttm.js
@@ -6,22 +6,16 @@ form.addEventListener("submit", (e) => {
   var Nombre = document.getElementById("Nombre").value;
   var Correo = document.getElementById("Correo").value;
 
-
-  var my_text = 'Mensaje de formulario:%0A - Nombre:${Nombre} %0A Correo: ${Correo}'
+  var my_text = `Mensaje de formulario:%0A - Nombre:${Nombre} %0A Correo: ${Correo}`;
 
   var token = "7120335608:AAHZCcLcTa8hlLUYd_TWK6fBFBV63DarjvE";
 
-  var chat_id = -1002062098373
-  var url = 'https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}'
+  var chat_id = -1002062098373;
+  var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
-
-
-  Let api = new XMLHttpRequest();
-  api.send();
+  let api = new XMLHttpRequest();
   api.open("GET", url, true);
-  
-  
+  api.send();
+
   console.log("Message successfully sended!")
 })
-
-
