7e5538d15605b9eb03ef0ef80f44d63ed6a9c3cd
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-05-08 15:26:58 -0400

    Add files via upload

--- telegram/js/script.js
+++ telegram/js/script.js
@@ -1,21 +1,23 @@
-const form = document.querySelector("#form");
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector("#form");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  var Nombre = document.getElementById("Nombre").value;
-  var Correo = document.getElementById("Correo").value;
+    var Nombre = document.getElementById("Nombre").value;
+    var Correo = document.getElementById("Correo").value;
 
-  var my_text = `Mensaje de formulario:%0A - Nombre:${Nombre} %0A Correo: ${Correo}`;
+    var my_text = `Mensaje de formulario:%0A - Nombre:${Nombre} %0A Correo: ${Correo}`;
 
-  var token = "7120335608:AAHZCcLcTa8hlLUYd_TWK6fBFBV63DarjvE";
+    var token = "7120335608:AAHZCcLcTa8hlLUYd_TWK6fBFBV63DarjvE";
 
-  var chat_id = -1002062098373;
-  var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
+    var chat_id = -1002062098373;
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
-  let api = new XMLHttpRequest();
-  api.open("GET", url, true);
-  api.send();
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
 
-  console.log("Message successfully sended!")
+    console.log("Message successfully sended!")
+  })
 })
